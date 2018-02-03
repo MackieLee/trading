2b42c434931f7654d8e6a3e1539d6c88f100c398
mackie <mackielee233@gmail.com>
2018-02-03 20:01:23 +0800

    lllll

--- src/views/fagui/Detail.vue
+++ src/views/fagui/Detail.vue
@@ -3,7 +3,7 @@
     <div class="cur-posi" id="top">
       <p>
         <i></i>当前位置 : &nbsp;
-        <router-link to="/home">九鼎财税</router-link>&nbsp;&gt;&nbsp;详细内容</p>
+        <router-link to="/home">九鼎财税</router-link>&nbsp;&gt;&nbsp;{{ content.name }}</p>
     </div>
     <div class="container">
       <div class="clearfix main-title ctr">
